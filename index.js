8cf8a4afc6e8920f332a2b4323c037e50776b199
chandransh <pandeychandransh@gmail.com>
2021-11-27 09:03:31 +0530

    fixed undefined returning from eval function displaying on screen

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ let screen = document.getElementById('screen')
 buttons = document.querySelectorAll('button');
 let screenValue = '';
 
+let tippu = false;
+
  for (items of buttons){
      items.addEventListener ('click', (e)=>{
          buttonText = e.target.innerText;
          console.log("button text is", buttonText)
+         console.log(tippu)
 
          if(buttonText === 'X'){
              buttonText = '*';
@@ -26,11 +29,24 @@ let screenValue = '';
          }
          
          else if(buttonText == '='){
-             screen.value = eval(screenValue);
-             screenValue = screen.value;
+            if (!tippu) {
+            screenValue = eval(screenValue);
+             screenValue ? screen.value = screenValue : screenValue = screen.value = "";
+            //  screenValue = screen.value;
+            tippu = !tippu;
+            }
+            else{
+                screenValue = eval(screenValue);
+             screenValue ? screen.value = screenValue : screenValue = screen.value = "";
+            }
          }else{
-             screenValue += buttonText;
-             screen.value = screenValue; 
+            if(tippu){
+                screenValue = screen.value = buttonText;
+                tippu = !tippu;
+            }else {
+                screenValue += buttonText;
+                screen.value  = screenValue;
+            }
          }
      })
  }
